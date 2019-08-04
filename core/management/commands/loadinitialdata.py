b63e7d230de9d628468dfff0a0bad78ad5e9ddf0
Gitigi <stephengitigi@gmail.com>
2019-08-04 09:49:06 +0300

    added arguments to loadinitialdata command for reading only members or payments values

--- core/management/commands/loadinitialdata.py
+++ core/management/commands/loadinitialdata.py
@@ -5,11 +5,24 @@ from ._private_load_payment import LoadPayment
 class Command(BaseCommand):
 	help = 'Loads initial data from 2016 to mid 2019'
 
+	def add_arguments(self,parser):
+		parser.add_argument('-m','--members',action='store_true',help='load only members information')
+		parser.add_argument('-p','--payments',action='store_true',help='load only payments information')
+
 	def handle(self,*args,**options):
 		self.stdout.write('loading data')
 		member_loader = LoadMembers()
 		payment_loader = LoadPayment()
-		self.stdout.write(self.style.SUCCESS('loading members...'))
-		member_loader.load()
-		self.stdout.write(self.style.SUCCESS('loading payments...'))
-		payment_loader.load()
+		members_only = options['members']
+		payments_only = options['payments']
+		if members_only:
+			self.stdout.write(self.style.SUCCESS('loading members...'))
+			member_loader.load()
+		elif payments_only:
+			self.stdout.write(self.style.SUCCESS('loading payments...'))
+			payment_loader.load()
+		else:
+			self.stdout.write(self.style.SUCCESS('loading members...'))
+			member_loader.load()
+			self.stdout.write(self.style.SUCCESS('loading payments...'))
+			payment_loader.load()
